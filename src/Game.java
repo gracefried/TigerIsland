2bec91f92bde13461b809dd5ffb35c37bcca663b
gonzalonunez <gonzi@tcpmiami.com>
2017-03-21 19:12:42 -0400

    Began player interaction. Missing rules from integrated components

--- src/Game.java
+++ src/Game.java
@@ -3,16 +3,37 @@
  */
 
 public class Game {
+    private Board board;
+
     private Player[] players;
 
-    public Game(Player[] players) {
+    public Game(Player... players) {
         this.players = players;
+        board = new Board();
     }
 
     public void runGameLoop() {
-        for (Player player : players) {
-            GameAction action = player.action();
-            //TODO: perform player actions here. Provide a new tile, and current board.
+        while (players.length != 0) {
+            int idx = 1;
+            for (Player player : players) {
+                System.out.println("* Player " + idx + "'s turn:");
+
+                GameActionPerformer actionPerformer = player.getGameActionPerformer();
+
+                Board currentBoard = board; // Do we need a copy here? I don't want to pass a modifiable reference of our Board...
+                Tile tile = new Tile(TerrainType.JUNGLE, TerrainType.GRASSLANDS, TerrainType.VOLCANO); //TODO: draw tile instead
+
+                //TODO: Confirm that requested tile placement is valid
+                TileCoordinate tileCoordinate = actionPerformer.tileAction(tile, currentBoard);
+
+                //TODO: If valid, place tile at this coordinate
+
+                //TODO: Confirm that the desired build move is valid
+                currentBoard = board;
+                actionPerformer.buildAction(tile, currentBoard);
+
+                idx++;
+            }
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,13 @@
 import javax.swing.*;
 
 public class Player {
-    public Player() {}
+    private GameActionPerformer gameActionPerformer;
 
-    public GameAction action() {
-        // Given a tile and the current board, what does the player do?
-        // This means we need to display the tile and the board to the user inside of these functions,
-        // then receive input on where they want to place the tile,
-        // then enforce that they placed the tile validly,
-        // then receive input on what build action they want to do,
-        // then make them perform a valid build action
-        //TODO: We need to get input from the "user" for these actions...
+    public Player(GameActionPerformer actionPerformer) {
+        gameActionPerformer = actionPerformer;
+    }
 
-        GameAction.TileAction tileAction = (tile, board) -> {
-            System.out.println("Did tile action...");
-            return new TileCoordinate(0, 0);
-        };
-
-        GameAction.BuildAction buildAction = (tile, board) -> {
-            System.out.println("Do build action...");
-        };
-
-        return new GameAction(tileAction, buildAction);
+    public GameActionPerformer getGameActionPerformer() {
+        return gameActionPerformer;
     }
 }
