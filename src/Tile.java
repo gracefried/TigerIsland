4347212c8c5f7b0cba9ab04e110d47288ce2bf2b
hsuj <ofa.hsuj@gmail.com>
2017-03-21 20:40:47 -0400

    Created new rotation methods in Tile class. Also created new enum that describes the orientation of the Tile i.e. TOPHEAVY and BOTTOMHEAVY. Moreover, changed references of top to middle.

--- src/Tile.java
+++ src/Tile.java
@@ -1,23 +1,55 @@
 public class Tile {
-    private Hexagon topHex;
+    private Hexagon middleHex;
     private Hexagon rightHex;
     private Hexagon leftHex;
+    private TileOrientationType tileOrientation;
 
-    public Tile(TerrainType leftType, TerrainType rightType, TerrainType topType) {
-        topHex = new Hexagon(topType);
+    public Tile(TerrainType leftType, TerrainType rightType, TerrainType middleType) {
+        middleHex = new Hexagon(middleType);
         rightHex = new Hexagon(rightType);
         leftHex = new Hexagon(leftType);
+        tileOrientation = TileOrientationType.TOPHEAVY;
     }
 
     public TerrainType getTerrainTypeForPosition(HexagonPosition position) {
         switch (position) {
-            case TOP:
-                return topHex.getTerrainType();
+            case MIDDLE:
+                return middleHex.getTerrainType();
             case RIGHT:
                 return rightHex.getTerrainType();
             case LEFT:
                 return leftHex.getTerrainType();
         }
         return null;
+    }
+
+    public void clockiseRotation() {
+        //swap L and M hexagons
+        if(tileOrientation == TileOrientationType.TOPHEAVY) {
+            Hexagon toBeMiddleHex = leftHex;
+            leftHex = middleHex;
+            middleHex = toBeMiddleHex;
+        }
+        //swap swap M and R hexagons
+        else if(tileOrientation == TileOrientationType.BOTTOMHEAVY) {
+            Hexagon toBeMiddleHex = rightHex;
+            rightHex = middleHex;
+            middleHex = toBeMiddleHex;
+        }
+    }
+
+    public void counterClockwiseRotation() {
+        //swap M and R hexagons
+        if(tileOrientation == TileOrientationType.TOPHEAVY) {
+            Hexagon toBeMiddleHex = rightHex;
+            rightHex = middleHex;
+            middleHex = toBeMiddleHex;
+        }
+        //swap L and M hexagons
+        else if(tileOrientation == TileOrientationType.BOTTOMHEAVY) {
+            Hexagon toBeMiddleHex = leftHex;
+            leftHex = middleHex;
+            middleHex = toBeMiddleHex;
+        }
     }
 }
---
+++ src/TileOrientationType.java
@@ -0,0 +1,3 @@
+public enum TileOrientationType {
+    TOPHEAVY, BOTTOMHEAVY
+}
