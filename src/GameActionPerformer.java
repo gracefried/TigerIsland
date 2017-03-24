6c149104932c31eb8ee7f2e7daae09ffa1ee57e6
Gonzalo Nuñez <gonzi@tcpmiami.com>
2017-03-24 10:30:12 -0400

    Removed outdated comment from GameActionPerformer

--- src/GameActionPerformer.java
+++ src/GameActionPerformer.java
@@ -3,5 +3,5 @@
  */
 public interface GameActionPerformer {
     Coordinate tileAction(Tile tile, Board board);
-    BuildAction buildAction(Board board); // this is void for now but we'll probably expect some other return type
+    BuildAction buildAction(Board board);
 }
